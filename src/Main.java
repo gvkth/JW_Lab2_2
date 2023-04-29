f678a8dfae008016e58e0f9af83574e3adabe052
cuongvk <cuongvk.hcm@vnpt.vn>
2023-04-29 11:38:08 +0700

    Done Lab 2.1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        /*BaseballCoach theCoach = new BaseballCoach();
+        System.out.println(theCoach.getDailyWorkout());*/
+
+        Coach theCoach = new BaseballCoach();
+
+        //use the object
+        System.out.println(theCoach.getDailyWorkout());
+    }
+}
